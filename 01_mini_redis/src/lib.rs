e73c7ff7d65522cb56e1b845e43fa7a014dae61d
qyuzh <qyuzhang@139.com>
2023-10-13 19:47:07 +0800

    feat(mini-redis): refactor

--- 01_mini_redis/src/lib.rs
+++ 01_mini_redis/src/lib.rs
@@ -1,11 +1,11 @@
-pub use constants::*;
+pub use config::*;
 
 pub mod server;
 mod db;
 mod connection;
 mod cmd;
 mod frame;
-mod constants;
+mod config;
 mod parse;
 
 pub type Error = Box<dyn std::error::Error + Send + Sync>;

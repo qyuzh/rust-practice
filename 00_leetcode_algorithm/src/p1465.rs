116939588aeac5ad34c8ceb0668c361b6ec36fa9
qyuzh <qyuzhang@139.com>
2023-10-27 06:21:30 +0800

    refactor(leetcode): p1465

--- 00_leetcode_algorithm/src/p1465.rs
+++ 00_leetcode_algorithm/src/p1465.rs
@@ -1,20 +1,16 @@
-/// 
+/// sort
 pub fn max_area(h: i32, w: i32, horizontal_cuts: Vec<i32>, vertical_cuts: Vec<i32>) -> i32 {
-    let mut hc = horizontal_cuts;
-    hc.sort_unstable();
-    let mut h_max = hc[0];
-    for i in 0..hc.len() - 1 {
-        h_max = h_max.max(hc[i + 1] - hc[i]);
-    }
-    h_max = h_max.max(h - hc[hc.len() - 1]);
-
-    let mut vc = vertical_cuts;
-    vc.sort_unstable();
-    let mut v_max = vc[0];
-    for i in 0..vc.len() - 1 {
-        v_max = v_max.max(vc[i + 1] - vc[i]);
-    }
-    v_max = v_max.max(w - vc[vc.len() - 1]);
-
+    let mut h_max = get_max_span(horizontal_cuts, h);
+    let mut v_max = get_max_span(vertical_cuts, w);
     ((h_max as i64 * v_max as i64) % (1e9 as i64 + 7)) as i32
+}
+
+fn get_max_span(mut cuts: Vec<i32>, upper: i32) -> i32 {
+    cuts.sort_unstable();
+    let mut max = cuts[0];
+    for i in 0..cuts.len() - 1 {
+        max = max.max(cuts[i + 1] - cuts[i]);
+    }
+    max =max.max(upper - cuts[cuts.len() - 1]);
+    max
 }

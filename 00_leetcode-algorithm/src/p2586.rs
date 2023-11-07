0b394eaed9b494e93ea07c0f87acb1066f22b32b
qyuzh <qyuzhang@139.com>
2023-11-07 12:01:22 +0800

    feat(leetcode): p2586

---
+++ 00_leetcode-algorithm/src/p2586.rs
@@ -0,0 +1,13 @@
+pub fn vowel_strings(words: Vec<String>, left: i32, right: i32) -> i32 {
+    let vowels = [b'a', b'e', b'i', b'o', b'u'];
+    let mut ans = 0;
+    for i in left..=right {
+        let w = &words[i as usize];
+        if vowels.contains(&w.as_bytes()[0])
+            && vowels.contains(&w.as_bytes()[w.as_bytes().len() - 1])
+        {
+            ans += 1;
+        }
+    }
+    ans
+}

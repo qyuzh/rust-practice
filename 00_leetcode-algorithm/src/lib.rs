7c80abeddca9c9c0394d28d9e9f72ecdba1fe72d
qyuzh <qyuzhang@139.com>
2024-03-30 19:22:15 +0800

    feat(leetcode): p2952

--- 00_leetcode-algorithm/src/lib.rs
+++ 00_leetcode-algorithm/src/lib.rs
@@ -75,6 +75,7 @@ pub mod p275;
 pub mod p2760;
 pub mod p2807;
 pub mod p2824;
+pub mod p2952;
 pub mod p299;
 pub mod p318;
 pub mod p322;
---
+++ 00_leetcode-algorithm/src/p2952.rs
@@ -0,0 +1,33 @@
+/// runs in O(n + nlogn)/O(1), in which n = coins.len()
+pub fn minimum_added_coins(mut coins: Vec<i32>, target: i32) -> i32 {
+    coins.sort_unstable(); // O(nlogn)
+
+    let mut ans = 0;
+
+    let mut ob = 0;
+    let mut i = 0;
+    while i < coins.len() {
+        if ob >= target {
+            return ans;
+        }
+        if coins[i] <= ob + 1 {
+            ob += coins[i];
+            i += 1;
+        } else {
+            ob += ob + 1;
+            ans += 1;
+        }
+    }
+
+    if ob >= target {
+        return ans;
+    }
+
+    // Could be optimized to O(1)
+    while ob < target {
+        ob += ob + 1;
+        ans += 1;
+    }
+
+    ans
+}

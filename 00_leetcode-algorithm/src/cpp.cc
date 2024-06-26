44dc17a338069c1aa796268b7c9ab8446ae71987
qyuzh <qingyu.zhang@stepai.cn>
2024-06-26 09:09:23 +0800

    feat(leetcode): p2741

--- 00_leetcode-algorithm/src/cpp.cc
+++ 00_leetcode-algorithm/src/cpp.cc
@@ -2,6 +2,40 @@
 
 using namespace std;
 
+class Solution2741 {
+    const long long MOD = 1'000'000'007;
+
+  public:
+    int specialPerm(vector<int> &nums) {
+        int n = nums.size();
+        int u = (1 << n) - 1;
+        vector<vector<long long>> memo(u, vector<long long>(n, -1));
+        auto dfs = [&](auto &dfs, int s, int i) -> long long {
+            if (s == 0) {
+                // find a solution
+                return 1;
+            }
+            auto &res = memo[s][i];
+            if (res != -1) {
+                return res;
+            }
+            res = 0;
+            for (int j = 0; j < n; ++j) {
+                if ((s >> j & 1) &&
+                    (nums[i] % nums[j] == 0 || nums[j] % nums[i] == 0)) {
+                    res = (res + dfs(dfs, s ^ (1 << j), j)) % MOD;
+                }
+            }
+            return res;
+        };
+        long long ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = (ans + dfs(dfs, u ^ (1 << i), i)) % MOD;
+        }
+        return ans;
+    }
+};
+
 class Solution520 {
   public:
     bool detectCapitalUse(string world) {
